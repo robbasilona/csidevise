504e665f534f5b761db66aa5b2a47bf41a6889ea
Rob Basilona <jbbasilona@up.edu.ph>
2017-02-01 15:59:30 +0800

    More Repos, More chances of winning.

--- mobile/src/pages/contact/contact.ts
+++ mobile/src/pages/contact/contact.ts
@@ -71,6 +71,69 @@ export class ContactPage {
         };
       });
     }
+    else if(this.trend == 'evacuation'){
+      this.api.loadCenters(0).then(data => {
+        let names = [];
+        let cvalues = [];
+        let tvalues = [];
+        for (let i in data) {
+          names[i] = data[i].name;
+          cvalues[i] = data[i].quantity;
+          tvalues[i] = data[i].capacity;
+        }
+        this.options = {
+          chart: {
+            type: 'spline',
+            width: this.width - 32,
+            height: this.height - 204,
+            backgroundColor: '#FFFFFF'
+          },
+          title: {
+            text: ''
+          },
+          // subtitle: {
+          //   text: 'Source: WorldClimate.com'
+          // },
+          xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+          },
+          yAxis: {
+            title: {
+              text: ''
+            }
+            // ,
+            // labels: {
+            //   formatter: function () {
+            //     return this.value + '°';
+            //   }
+            // }
+          },
+          plotOptions: {
+            spline: {
+              marker: {
+                radius: 4,
+                lineColor: '#666666',
+                lineWidth: 1
+              }
+            }
+          },
+          series: [
+          {
+            name: names[0],
+            // marker: {
+            //   symbol: 'square'
+            // },
+            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+          }, {
+            name: names[1],
+            // marker: {
+            //   symbol: 'diamond'
+            // },
+            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+          }]
+        }
+      });
+    }
   }
 
 }
--- railsapi/app/models/evac_center.rb
+++ railsapi/app/models/evac_center.rb
@@ -4,7 +4,8 @@ class EvacCenter < ApplicationRecord
 	acts_as_mappable :default_units => :kms, :default_formula => :sphere, :lat_column_name => :latitude, :lng_column_name => :longitude
 
   def score(src)
-		dist = self.distance_to(src, :units => :kms)
+		self.dist = self.distance_to(src, :units => :kms).round(2)
+		self.save
 		per = self.quantity.to_f / self.capacity.to_f
 		dist + per * dist
   end
@@ -18,8 +19,12 @@ class EvacCenter < ApplicationRecord
 		src = EvacCenter.new(latitude: loc.lat, longitude: loc.lng, name: 'User')
 		results =  EvacCenter.within(20, :origin=>[loc.lat, loc.lng]).sort { |x, y| x.score(src) <=> y.score(src) }
 		response = 'We recommend you to take shelter on these areas: '
-		results.each do |res|
-			response += res.name + ':' + res.quantity.to_s + '/' + res.capacity.to_s + ' - '
+		if results == []
+			response = "Sorry, we can't find any suitable location near you. Please contact your local government units. "
+		else
+			results[0..4].each do |res|
+				response += res.name + ':' + res.quantity.to_s + '/' + res.capacity.to_s + ' - '
+			end
 		end
 		response += 'Keep safe! RefuGeo'
 	end
